c57a46b5991d13bdcd02d38d68784d1850642417
Raphael <graphael.dr@gmail.com>
2024-02-23 12:20:28 -0600

    testing db connection

---
+++ db_test.php
@@ -0,0 +1,25 @@
+
+<?php 
+        
+  $servername = "localhost";
+  $username = "root";
+  $dbname = "registration";
+
+  $conn = mysqli_connect($servername, $username,  "", $dbname);
+
+  if(!$conn){
+      die("Connection failed: " . mysqli_connect_error()); 
+  } 
+
+  echo "Connected to database";
+  
+  $sql = "INSERT INTO users (id, fname, lname, email, password)
+  VALUES (2, 'John', 'Doe', 'john@gmail.com', 'johnpass123')";
+
+  if(mysqli_query($conn, $sql)){
+    echo "Insert Successful";
+  } else {
+    echo "Something went wrong";
+  }
+        
+?>
