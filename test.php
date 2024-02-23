b83d9fa0cfbc2df4be92461b36cf7eeadd940302
Raphael <graphael.dr@gmail.com>
2024-02-23 15:49:42 -0600

    test sql queries

--- test.php
+++ test.php
@@ -1,4 +1,16 @@
 <?php
-   echo "<script>alert('Insert Successful')</script>";
-   echo 'hey';
+   // echo "<script>alert('Insert Successful')</script>";
+
+   $conn = mysqli_connect('localhost', 'root',  "", 'registration');
+
+   $sql = "SELECT * FROM users WHERE email='john@gmail.com'";
+
+   $result = mysqli_fetch_array(mysqli_query($conn, $sql));
+   // $result = mysqli_fetch_array($result);
+
+   // foreach($row as $val){
+   //    echo $val . '<br>';
+   // }
+
+   echo $result['email'];
 ?>
