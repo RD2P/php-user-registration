4d503b3884799943faba2bf048fe3841ed1bcc39
Raphael <graphael.dr@gmail.com>
2024-02-24 15:08:16 -0600

    login validation

--- login.php
+++ login.php
@@ -6,14 +6,80 @@
   <title>Login</title>
 </head>
 <body>
+  <?php
+
+  session_start();
+
+  $conn = mysqli_connect("localhost", "root", "", 'registration') or die(mysqli_connect_error());
+
+  if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $valid = true;
+
+    $email = $password = "";
+    $emailErr = $passwordErr = "";
+
+    //----validate login fields
+
+    //Email validation
+    if(empty($_POST['email'])){
+      $emailErr = "Please provide email";
+      $valid = false;
+    } else {
+      $email = $_POST['email'];
+      if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Invalid email format";
+        $valid = false;
+      }
+    }
+
+    //Password validation
+    if(empty($_POST['password'])){
+      $passwordErr = "Please provide password";
+      $valid = false;
+    } else {
+      $password = $_POST['password'];
+    }
+
+    if($valid) {
+      //escape special chars
+      $email = mysqli_real_escape_string($conn, $_POST['email']);
+      $password = mysqli_real_escape_string($conn, $_POST['password']);
+      
+      $query = mysqli_query($conn, "SELECT * FROM users WHERE email='$email'");
+      
+      $result = mysqli_fetch_assoc($query);
+
+      //check if there is row with matching email
+      if($result){
+        //check if password matches
+        if($password == $result['password']){
+          $_SESSION['fname'] = $result['fname'];
+          $_SESSION['email'] = $email;
+          header("Location: dashboard.php");
+        } else {
+          $passwordErr = "Incorrect password";
+          $valid = false;
+        }
+      } else {
+        $emailErr = "Incorrect email";
+        $valid = false;
+      }
+    }
+  }
+
+  ?>
   <h2>LOGIN</h2>
-  <form method="POST" action="check_login.php">
+  <form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+
     <label for="email">Email</label><br>
-    <input type="email" id="email" required/>
+    <input type="text" id="email" name="email" value="<?php echo $email ?? ''; ?>"/>
+    <span>* <?php echo $emailErr ?? '';?></span>
     <br>
     <label for="Password">Password</label><br>
-    <input type="password" id="password" required/>
+    <input type="password" id="password" name="password"/>
+    <span>* <?php echo $passwordErr ?? '';?></span>
     <br><br>
+
     <input type="submit" value="Login"/>
   </form>
   
