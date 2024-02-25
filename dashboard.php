d96af4df3cb3b1ff20ce54266c675857752b0958
Raphael <graphael.dr@gmail.com>
2024-02-25 17:24:18 -0600

    add new post form

--- dashboard.php
+++ dashboard.php
@@ -6,13 +6,56 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <script src="writePost.js"></script>
   <title>Welcome</title>
 </head>
+
 <body>
-  Hey <?php echo $_SESSION['fname'];?> welcome to your dashboard <br>
-  <form action="dashboard.php" method="POST">
-    <input type="submit" name="logout" value="Logout"/>
-  </form>
+  <header class="flex justify-between align-center bg-slate-50 py-4 px-4">
+    <h2 class="p-0"><?php echo $_SESSION['fname'];?></h2>
+    
+    <div>
+      <form action="dashboard.php" method="POST">
+        <input type="submit" name="logout" value="Logout" class="bg-green-200 p-2 hover:bg-green-100 cursor-pointer"/>
+      </form>
+    </div>
+  </header>
+  <section 
+    id="write-post-section"
+    class="flex flex-col justify-center mt-10 max-w-96 mx-auto">
+    <button 
+      id="write-post-btn"
+      class="bg-green-300 p-3 hover:bg-green-100 cursor-pointer text-lg mb-3"
+      onclick="showPostModal()">
+      Write a post
+    </button>
+    
+    <form id="write-post-form" class="flex flex-col justify-between">
+      <div class="">
+        <textarea 
+          placeholder="Write a new post..."
+          class="border border-1 p-2 text-lg w-full min-h-32"></textarea>
+      </div>
+      <div class="flex justify-end">
+        <input
+          type="submit"
+          value="Post"
+          class="bg-green-300 p-3 hover:bg-green-100 cursor-pointer text-lg mb-3"/>
+      </div>
+
+
+    </form>
+
+  </section>
+  <main >
+
+  </main>
+
+
+
+
+
   <?php
     if(isset($_POST['logout'])){
       session_destroy();
