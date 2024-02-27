d4d77d348e8503e3901144d7792e5de3f31453e1
Raphael <graphael.dr@gmail.com>
2024-02-26 22:58:16 -0600

    template for posts

---
+++ post.php
@@ -0,0 +1,24 @@
+<!-- <?php 
+  echo 'test';
+  echo 'test';
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+<body>
+  <br><br>
+  <article class="flex flex-col max-w-xl mx-auto border border-t-2 p-4">
+    <section class="flex items-center">
+      <h4 class="font-bold text-lg text-green-700">Raphael</h4>
+      <span>&nbsp;&#183; Feb 25</span>
+    </section>
+    <section>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio ipsa eius dolore temporibus natus voluptas quasi repellat, nisi vel, quia deserunt in cum obcaecati distinctio reprehenderit harum aut odit odio?</section>
+  </article>
+</body>
+</html>
