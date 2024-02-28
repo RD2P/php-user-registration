25745b28bce7e56699a037fe6f5158e65635eaf9
Raphael <graphael.dr@gmail.com>
2024-02-27 19:51:35 -0600

    testing iteration through multiple arrays

--- test.php
+++ test.php
@@ -1,7 +1,12 @@
 <?php
    $names = array('Raphael', 'Taylor');
+   $dates = array("Feb 21", "Feb 29");
+   $posts = array("Hi I'm Raphael", "Hi I'm Tyalor");
 
-   foreach($names as $name) {
+   foreach($names as $key => $name) {
+
       echo "<h4 class='font-bold text-lg text-green-700'>{$name}</h4>";
+      echo "<span>&nbsp;&#183; {$dates[$key]}</span>";
+      echo "<section>{$posts[$key]}</section>";
    }
 ?>
