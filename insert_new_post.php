99de8618219e628c4d45085c79b28bc2b687f00c
Raphael <graphael.dr@gmail.com>
2024-02-28 15:25:44 -0600

    completed insert funcitonality

--- insert_new_post.php
+++ insert_new_post.php
@@ -1,10 +1,17 @@
 <?php 
-  // TODO add db connection to session upon login/registration
-  // session_start();
-  // $conn = $_SESSION['db_connection'];
-
+  session_start();
   $conn = mysqli_connect("localhost", "root", "", 'registration') or die(mysqli_connect_error());
 
   $post_text = $_POST['post_text'];
-  echo $post_text;
+  
+  $query_get_id = mysqli_query($conn, "SELECT id FROM users WHERE fname='{$_SESSION['fname']}'");
+  $res = mysqli_fetch_array($query_get_id);
+
+  $currentuser_id = $res[0];
+
+  $post_text_escaped = mysqli_real_escape_string($conn, $post_text);
+
+  $query = mysqli_query($conn, "INSERT INTO posts (`post`, `user_id_fk`) VALUES ('$post_text_escaped', '$currentuser_id')");
+
+  header("Location: dashboard.php");
 ?>
