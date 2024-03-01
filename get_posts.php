66ff35c2714810cc4f976c6baca882df030ce176
Raphael <graphael.dr@gmail.com>
2024-02-29 22:51:39 -0600

    cleanup comments

--- get_posts.php
+++ get_posts.php
@@ -1,17 +1,10 @@
-
 <?php 
    $conn = mysqli_connect("localhost", "root", "", 'registration') or die(mysqli_connect_error());
 
-   //returns sql object, turn into array
+   //returns sql object
    $all_posts = mysqli_query($conn, "SELECT * FROM posts");
 
-  //  $posts_array = mysqli_fetch_assoc($all_posts);
-
-  //  foreach($posts_array as $key => $row){
-  //   echo $key . ' --> ' . $row . '<br>';
-  //  }
    while($row = mysqli_fetch_assoc($all_posts)) {
       include 'post_template.php';
     }
-
 ?>
