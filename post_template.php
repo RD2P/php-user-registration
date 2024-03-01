a7e8379798fa29ca2d5900475e65d74cb004c41b
Raphael <graphael.dr@gmail.com>
2024-02-29 23:22:52 -0600

    add username on top of post

--- post_template.php
+++ post_template.php
@@ -1,7 +1,12 @@
 <?php 
+  //find fname of $row['user_id_fk']
+  $result = mysqli_query($conn, "SELECT fname FROM users WHERE id={$row['user_id_fk']}");
+
+  $username = mysqli_fetch_array($result);
+
   echo "<article class='flex flex-col max-w-xl mx-auto border border-t-2 p-4'>
-          <section class='flex items-center>
-            <h4 class='font-semibold text-lg text-green-700'>{$row['user_id_fk']}</h4>
+          <section class='flex items-center'>
+            <h4 class='font-semibold text-lg text-green-700'>{$username[0]}</h4>
           </section>
           <section>{$row['post']}</section>
         </article>";
