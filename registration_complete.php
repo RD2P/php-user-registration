73fde293438ec8604efe9e81a712fc4a93326b28
Raphael <graphael.dr@gmail.com>
2024-02-29 23:25:14 -0600

    add styles

--- registration_complete.php
+++ registration_complete.php
@@ -3,10 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <title>Registration successful</title>
 </head>
 <body>
-  Registration successful! <br>
-  <a href="dashboard.php">Go to my dashboard</a>
+  <div class="max-w-80 mx-auto mt-14">
+    <h2 class="my-3">Registration successful!</h2>
+    <a href="dashboard.php" class="text-lg bg-green-200 p-2">Go to my dashboard</a>
+  </div>
 </body>
 </html>
